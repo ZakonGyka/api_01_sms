cad72da73c0bb71472e9606c56bf36204d6392e4
Петр Лукахин <zakongyka@yandex.ru>
2021-01-05 01:23:21 +0300

    fix| clean code

--- homework.py
+++ homework.py
@@ -1,43 +1,41 @@
+import os
 import time
 
 import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
 
-# здесь проинициализируйте Client
+load_dotenv()
+
 # Your Account SID from twilio.com/console
-account_sid = "AC6ae68f31f584a4c28bda6964a65e8b36"
+account_sid = os.getenv('ACCOUNT_SID')
 # Your Auth Token from twilio.com/console
-auth_token = "0f1c583706b7ea68dce7b7d9756e54d0"
+auth_token = os.getenv('AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
 
 
 def get_status(user_id):
-    print('++++++++')
+    access_token = os.getenv('access_token')
     params = {
         "user_id": user_id,
         "v": "5.92",
-        "access_token": "a894afd651dea9c7036f4b41da2de35b027fa0b4314293d5869c1a870a77cc420d833f9f8367ffc1bcb12",
+        "access_token": access_token,
     }
     user_information = requests.post('https://api.vk.com/method/users.get?fields=online,status', params=params)
     return user_information.json()['response'][0]['online']
-    # return ...  # Верните статус пользователя в ВК
 
-
-# print(get_status(20316432))
 
 def send_sms(sms_text):
     message = client.messages.create(
-        to="+79774208055",
-        from_="+15122706027",
-        # media_url='https://demo.twilio.com/owl.png',
+        from_=os.getenv('NUMBER_FROM'),
+        to=os.getenv('NUMBER_TO'),
         body=sms_text)
-    #print(message.sid)
     return message.sid  # Верните sid отправленного сообщения из Twilio
 
 
 if __name__ == '__main__':
-    vk_id = 531301803
+    vk_id = 20316432
     while True:
         if get_status(vk_id) == 1:
             send_sms(f'{vk_id} сейчас онлайн!')
